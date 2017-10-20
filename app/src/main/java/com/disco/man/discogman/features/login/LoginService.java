e52aac3e7cbedbf892e51dc951f741648174da7f
teo <ramones.teo@gmail.com>
2017-10-21 00:04:10 +0300

    Successful for get RequestToken

--- app/src/main/java/com/disco/man/discogman/features/login/LoginService.java
+++ app/src/main/java/com/disco/man/discogman/features/login/LoginService.java
@@ -1,10 +1,10 @@
 package com.disco.man.discogman.features.login;
 
 import io.reactivex.Single;
-import okhttp3.RequestBody;
-import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import okhttp3.ResponseBody;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 
 /**
  * Retrofit service for login screen.
@@ -12,7 +12,11 @@ import retrofit2.http.POST;
 
 public interface LoginService {
 
-    @POST("/oauth/request_token")
-    Single<Call<Void>> getRequestToken(@Body RequestBody requestBody);
+    @Headers({
+            "Content-Type: application/x-www-form-urlencoded",
+            "User-Agent: Discs/1.0 +https://jb.com"
+    })
+    @GET("/oauth/request_token")
+    Single<ResponseBody> getRequestToken(@Header("Authorization") String header);
 
 }
--- app/src/main/java/com/disco/man/discogman/features/login/LoginViewModel.java
+++ app/src/main/java/com/disco/man/discogman/features/login/LoginViewModel.java
@@ -6,13 +6,8 @@ import com.disco.man.discogman.BuildConfig;
 import com.disco.man.discogman.Constants;
 import com.disco.man.discogman.utils.schedulers.BaseSchedulerProvider;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import javax.inject.Inject;
 
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
 import timber.log.Timber;
 
 /**
@@ -31,7 +26,7 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void getRequestToken() {
-        loginService.getRequestToken(createRequestBodyForRequestToken())
+        loginService.getRequestToken(createHeaderForRequestToken())
                 .subscribeOn(schedulerProvider.io())
                 .observeOn(schedulerProvider.androidMainThread())
                 .subscribe((aVoid, throwable) -> {
@@ -41,23 +36,16 @@ public class LoginViewModel extends ViewModel {
                 });
     }
 
-    private RequestBody createRequestBodyForRequestToken() {
-        JSONObject jsonObject = null;
-        try {
-            jsonObject = new JSONObject()
-                    .put("oauth_consumer_key", BuildConfig.CONSUMER_KEY)
-                    .put("oauth_nonce", String.valueOf(System.currentTimeMillis()))
-                    .put("oauth_signature", BuildConfig.CONSUMER_SECRET_KEY)
-                    .put("oauth_signature_method", "plaintext")
-                    .put("oauth_timestamp", String.valueOf(System.currentTimeMillis()))
-                    .put("oauth_callback", Constants.OAUTH_CALLBACK);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+    private String createHeaderForRequestToken() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("OAuth ").append("oauth_consumer_key=\"" + BuildConfig.CONSUMER_KEY + "\",");
+        stringBuilder.append("oauth_nonce=\"").append(String.valueOf(System.currentTimeMillis()) + "\",");
+        stringBuilder.append("oauth_signature=\"" + BuildConfig.CONSUMER_SECRET_KEY + "&\",");
+        stringBuilder.append("oauth_signature_method=\"" + "PLAINTEXT" + "\",");
+        stringBuilder.append("oauth_timestamp=\"").append(String.valueOf(System.currentTimeMillis()) + "\",");
+        stringBuilder.append("oauth_callback=\"" + Constants.OAUTH_CALLBACK + "\"");
 
-        return RequestBody.create(
-                MediaType.parse("application/x-www-form-urlencoded"),
-                jsonObject.toString());
+        return stringBuilder.toString();
     }
 
 }
